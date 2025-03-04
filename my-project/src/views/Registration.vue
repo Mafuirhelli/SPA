661e255c3bf4d3a3715f8efa04f46915a2b72bed
Mafuirhelli <61899009+Mafuirhelli@users.noreply.github.com>
2025-03-04 19:41:51 +0700

    login + sign up

--- my-project/src/views/Registration.vue
+++ my-project/src/views/Registration.vue
@@ -1,11 +1,35 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div>
+    <h1>Регистрация</h1>
+    <form @submit.prevent="register">
+      <input v-model="userData.fio" placeholder="ФИО" required />
+      <input v-model="userData.email" type="email" placeholder="Email" required />
+      <input v-model="userData.password" type="password" placeholder="Пароль" required />
+      <button type="submit">Зарегистрироваться</button>
+    </form>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<script>
+export default {
+  data() {
+    return {
+      userData: {
+        fio: '',
+        email: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        await this.$store.dispatch('register', this.userData);
+        this.$router.push('/');
+      } catch (error) {
+        alert(error.message);
+      }
+    },
+  },
+};
+</script>
